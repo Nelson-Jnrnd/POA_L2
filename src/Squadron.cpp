bfcaae8b3695ecb12af43d35c5324a6bf0f896f2
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-03-31 18:01:20 +0200

    Squadrons

--- src/Squadron.cpp
+++ src/Squadron.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Squadron.h"
+
+Squadron::Squadron(const std::string &name) : name(name) {}
+
+Squadron::Squadron(const Squadron &squadron) : name(squadron.name), ships(squadron.ships) {}
+
+Squadron::~Squadron() {
+    for (auto ship : ships) {
+        delete ship;
+    }
+}
+
+Squadron& Squadron::Squadron::operator=(const Squadron &squadron) {
+    if (this != &squadron) {
+        name = squadron.name;
+        ships = squadron.ships;
+    }
+    return *this;
+}
+
+void Squadron::addShip(Ship *ship) {
+    if(ship == nullptr) {
+        throw std::invalid_argument("Ship cannot be nullptr");
+    }
+    if(containsShip(ship)) {
+        throw std::invalid_argument("Ship already in squadron");
+    }
+    ships.push_back(ship);
+}
+
+void Squadron::setLeader(Ship *ship) {
+    if(ship == nullptr) {
+        throw std::invalid_argument("Ship cannot be nullptr");
+    }
+    if(!containsShip(ship)) {
+        throw std::invalid_argument("Ship not in squadron");
+    }
+    leader = ship;
+}
+
+void Squadron::removeLeader() {
+    if(leader == nullptr) {
+        throw std::invalid_argument("Squadron has no leader");
+    }
+    leader = nullptr;
+}
+
--- src/Squadron.h
+++ src/Squadron.h
@@ -5,9 +5,35 @@
 #ifndef POA_L2_SQUADRON_H
 #define POA_L2_SQUADRON_H
 
+#include <vector>
+#include "Ship.h"
 
 class Squadron {
-    
+    /// Ships contained in the squadron
+    std::vector<Ship*> ships;
+
+    /// Leader of the squadron
+    Ship *leader;
+
+    /// Name of the squadron
+    std::string name;
+public:
+    Squadron(const std::string &name);
+    Squadron(const Squadron& squadron);
+    Squadron& operator=(const Squadron& squadron);
+    ~Squadron();
+    void addShip(Ship* ship);
+    void removeShip(Ship* ship);
+    bool containsShip(Ship* ship);
+    unsigned int getSize() const;
+    void print() const; 
+
+    /// Set leader of the squadron
+    void setLeader(Ship* ship);
+    /// Remove leader of the squadron
+    void removeLeader();
+    /// Get leader of the squadron
+    Ship* getLeader() const;
 
 };
 
